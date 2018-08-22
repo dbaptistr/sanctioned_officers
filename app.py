ab91d1f86cf34b40bf801e96f0d962f3d4c07f86
dbaptistr <dianadbr@gmail.com>
2018-08-22 19:12:42 -0400

    data app

--- app.py
+++ app.py
@@ -14,16 +14,12 @@ class Official(db.Model):
   officialNumber = db.Column(db.String, primary_key=True)
 
 @app.route("/")
-def hello():
-  return "This is a website about government advertisement"
-
-@app.route("/officials/")
 def list():
   officials = Official.query.all()
   return render_template("list.html", officials=officials)
 
 
-@app.route("/officials/<officialNumber>/")
+@app.route("/<officialNumber>/")
 def show(officialNumber):
   official = Official.query.filter_by(officialNumber=officialNumber).first()
   return render_template("show.html", official=official) 
